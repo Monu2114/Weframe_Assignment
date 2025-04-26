201e3a42dc43cb0218362f5bfa606a29e17a5dc6
Monu_2114 <monu.nanabala@gmail.com>
2025-04-26 12:23:56 +0530

    feedback

---
+++ src/components/Board/Feedback.jsx
@@ -0,0 +1,29 @@
+export default function Franchise() {
+  return (
+    <div className="w-[338px] max-w-4xl border-black border-2 p-4 rounded-xl flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
+        <h1 className="font-bold">Key Insights & Feedback</h1>
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-2 ">
+            <p className="font-semibold text-3xl">10%</p>
+            <p className="">Sales Growth</p>
+          </div>
+          <div className="flex flex-col justify-center items-center gap-2 ">
+            <img src="/performer.svg" alt="" className="w-[36px] h-[36px]" />
+
+            <button className="bg-[#D5F2FF80] rounded-3xl px-4 h-6 text-[#279DD4] font-semibold text-[12px]">
+              Top Performer
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr className=" w-full border-gray-200 rounded-sm" />
+      <div className="rounded-xl bg-[#F6F7FB] flex flex-col px-6 py-4">
+        <h2 className="font-semibold">Feedback</h2>
+        <li className="text-[#455468]">
+          Franchisees are requesting more detailed training materials.
+        </li>
+      </div>
+    </div>
+  );
+}
--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,22 +1,21 @@
 import AccountProgress from "../components/Board/AccountProgress";
 import Franchise from "./Board/Franchise";
 import Finance from "./Board/Finance";
+import Feedback from "./Board/Feedback";
+import Leads from "./Board/Leads";
 export default function Board() {
   return (
     <div className="mt-6 p-4 flex gap-8 h-1/2 w-full">
       <AccountProgress />
-      <div className="flex flex-col gap-6 w-[586px]">
+      <div className="flex flex-col gap-6">
         <Franchise />
         <Finance />
       </div>
-      {/* <div className="flex flex-1/2">
-        
-       
-      </div>
-      <div className="flex flex-1/2">
+
+      <div className="flex flex-col gap-6">
         <Feedback />
         <Leads />
-      </div> */}
+      </div>
     </div>
   );
 }
