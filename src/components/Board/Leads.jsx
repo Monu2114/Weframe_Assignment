c1360c29e7de3230062d1c81fb88d99c081da5e4
Monu_2114 <monu.nanabala@gmail.com>
2025-04-26 12:59:16 +0530

    prospect leads

--- src/components/Board/Leads.jsx
+++ src/components/Board/Leads.jsx
@@ -1,25 +1,28 @@
-export default function Finance() {
+export default function () {
+  const users = [
+    { name: "Wade Warren", Stage: "Initial Inquiry" },
+    { name: "Ava Wright", Stage: "Initial Inquiry" },
+    { name: "Cody Fisher", Stage: "Initial Inquiry" },
+  ];
   return (
-    <div className="w-[338px] h-[284px] max-w-4xl border-black border-2 p-4 rounded-xl flex flex-col gap-4">
+    <div className="w-full h-full max-w-4xl border-black border-2 p-6 rounded-xl flex flex-col gap-2">
       <div className="flex flex-col gap-4">
-        <h1 className="font-bold">Financial Wellbeing</h1>
-        <div className="flex justify-between">
-          <div className="flex flex-col">
-            <p className="font-semibold text-3xl">20</p>
-            <p className=" text-gray-600 ">Total Franchises</p>
-          </div>
-          <img src="/raise-2.svg" alt="" />
-        </div>
-        <hr className=" w-full border-gray-200 rounded-sm" />
-      </div>
-      <div className="w-full flex gap-2 p-2">
-        <div className="w-1/2 bg-gray-100 rounded-sm flex flex-col items-center gap-2 py-4 mb-2">
-          <p className="text-[#455468] font-semibold">Target</p>
-          <p className="font-semibold text-2xl">$500,000</p>
-        </div>
-        <div className="w-1/2 bg-gray-100 rounded-sm flex flex-col items-center gap-2 py-4 mb-2">
-          <p className="text-[#455468] font-semibold">Current</p>
-          <p className="font-semibold text-2xl">$450,000</p>
+        <h1 className="font-bold text-lg">Prospect Leads</h1>
+        <div className="ml-2 flex flex-col gap-2 rounded-xl">
+          {users.map((user, index) => (
+            <div
+              key={index}
+              className="bg-[#F6F7FB] rounded-sm flex justify-between p-2"
+            >
+              <div className="inline-flex items-start">
+                <div className="inline-flex">
+                  <img src={`/users/${index}.svg`} alt="" className="w-8 h-8" />
+                  {user.name}
+                </div>
+                Stage: {user.Stage}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
